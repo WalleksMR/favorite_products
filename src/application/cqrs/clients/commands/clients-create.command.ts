417970a8adb8e09d78ab5afe8c576aef3023afce
Walleks Miranda <walleksmiranda@gmail.com>
2025-05-25 00:11:23 -0300

    refactor: change validate to zod

--- src/application/cqrs/clients/commands/clients-create.command.ts
+++ src/application/cqrs/clients/commands/clients-create.command.ts
@@ -1,12 +1,24 @@
-import { IsEmail, IsString } from 'class-validator';
+import { z } from 'zod';
+
+import { AppError } from '@/application/errors';
+
+const ClientsCreateCommandSchema = z.object({
+  name: z.string().min(5, 'Nome deve ter no mínimo 5 caracteres'),
+  email: z.string().email('E-mail inválido'),
+});
+
+type ClientsCreateCommandProps = z.infer<typeof ClientsCreateCommandSchema>;
 
 export class ClientsCreateCommand {
-  @IsString()
   name: string;
-  @IsEmail()
   email: string;
 
-  constructor(props: ClientsCreateCommand) {
+  constructor(props: ClientsCreateCommandProps) {
+    const result = ClientsCreateCommandSchema.safeParse(props);
+    if (!result.success) {
+      const message = result.error.errors[0]?.message;
+      throw new AppError(message);
+    }
     this.name = props.name;
     this.email = props.email;
   }
--- src/application/cqrs/clients/commands/clients-delete.command.ts
+++ src/application/cqrs/clients/commands/clients-delete.command.ts
@@ -1,10 +1,20 @@
-import { IsUUID } from 'class-validator';
+import { z } from 'zod';
+
+import { AppError } from '@/application/errors';
+
+const ClientsDeleteCommandSchema = z.object({
+  id: z.string().uuid({ message: 'Id inválido' }),
+});
 
 export class ClientsDeleteCommand {
-  @IsUUID()
   id: string;
 
   constructor(id: string) {
+    const result = ClientsDeleteCommandSchema.safeParse({ id });
+    if (!result.success) {
+      const message = result.error.errors[0]?.message;
+      throw new AppError(message);
+    }
     this.id = id;
   }
 }
