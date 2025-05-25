f5c29c3129e7fa003bca12c180619c8a0802f0d8
Walleks Miranda <walleksmiranda@gmail.com>
2025-05-24 21:15:47 -0300

    feat: add seed

--- database/seeds/test/1748050242978-SeedFavoriteProducts.ts
+++ database/seeds/test/1748050242978-SeedFavoriteProducts.ts
@@ -1,11 +1,52 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { faker } from '@faker-js/faker';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
+import { Client, Product } from '@/domain/entities';
+import { PgClient } from '@/infrastructure/database/postgres/entities';
 export class SeedFavoriteProducts1748050242978 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const clientRepo = queryRunner.manager.getRepository(PgClient);
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    const clients = [
+      {
+        ...new Client({
+          email: 'johndoe@email.com',
+        }),
+        favoriteProducts: [],
+      },
+      {
+        ...new Client({
+          email: faker.internet.email(),
+        }),
+        favoriteProducts: [],
+      },
+      {
+        ...new Client({
+          email: faker.internet.email(),
+        }),
+        favoriteProducts: [],
+      },
+    ];
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    for (const client of clients) {
+      for (let i = 0; i < 5; i++) {
+        client.favoriteProducts.push(
+          new Product({
+            title: faker.commerce.productName(),
+            image: faker.image.imageUrl(),
+            price: Number(faker.commerce.price()),
+            review: faker.datatype.number({ min: 1, max: 5 }),
+          }),
+        );
+      }
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+    await clientRepo.save(clients);
+  }
 
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query('TRUNCATE TABLE favorite_products CASCADE');
+    queryRunner.query('TRUNCATE TABLE products CASCADE');
+  }
 }
